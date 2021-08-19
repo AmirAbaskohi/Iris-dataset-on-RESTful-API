2ffcd18f88b811b352aad7f6e8c9749d8b3eb1f1
TheRealRondon <amirhossein2324236@gmail.com>
2021-08-19 22:58:44 +0430

    Added data to servre.

--- api/server.py
+++ api/server.py
@@ -37,7 +37,76 @@ class Classifier(Resource):
 
         return {'result': classes[np.argmax(model.predict(np.array(values)), axis=1)[0]]}, 200
 
+class Data(Resource):
+    def get(self):
+        return {'data': data.to_dict()}
+
+    def post(self):
+
+        global data
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('sepal_length', required=True)
+        parser.add_argument('sepal_width', required=True)
+        parser.add_argument('petal_length', required=True)
+        parser.add_argument('petal_width', required=True)
+        args = parser.parse_args()
+
+        sepal_length = float(args["sepal_length"])
+        sepal_width = float(args["sepal_width"])
+        petal_length = float(args["petal_length"])
+        petal_width = float(args["petal_width"])
+
+        data = data.append({"sepal.length": sepal_length, "sepal.width": sepal_width,
+                             "petal.length": petal_length, "petal.width":petal_width}, ignore_index=True)
+
+        return {'result': 'New data added successfully'}, 200
+
+    def put(self):
+
+        global data
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('index', required=True)
+        parser.add_argument('sepal_length', required=True)
+        parser.add_argument('sepal_width', required=True)
+        parser.add_argument('petal_length', required=True)
+        parser.add_argument('petal_width', required=True)
+        args = parser.parse_args()
+
+        index = int(args["index"])
+        sepal_length = float(args["sepal_length"])
+        sepal_width = float(args["sepal_width"])
+        petal_length = float(args["petal_length"])
+        petal_width = float(args["petal_width"])
+        args = parser.parse_args()
+
+        if index in data.index:
+            data.loc[index, "sepal.length"] = sepal_length
+            data.loc[index, "sepal.width"] = sepal_width
+            data.loc[index, "petal.length"] = petal_length
+            data.loc[index, "petal.width"] = petal_width
+            return {'result': 'Data updated successfully'}, 200
+        else:
+            return {'result': 'Index not found'}, 400
+
+    def delete(self):
+
+        global data
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('index', required=True)
+        args = parser.parse_args()
+
+        index = int(args["index"])
+        if index in data.index:
+            data = data.drop(index) 
+            return {'result': 'Data deleted successfully'}, 200
+        else:
+            return {'result': 'Index not found'}, 400
+
 api.add_resource(Classifier, '/classifier')
+api.add_resource(Data, '/data')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
